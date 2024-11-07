fe35e600fef7d08e1779ffd08965718295a204c4
Nicolas <m.nicolas.henry@gmail.com>
2024-11-07 18:49:27 +0100

    Fix compilation

---
+++ src/main/java/com/tcgp/cartodex/CartodexApplication.java
@@ -0,0 +1,13 @@
+package com.tcgp.cartodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartodexApplication.class, args);
+	}
+
+}
